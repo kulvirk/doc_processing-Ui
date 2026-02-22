92e56bcedb95ab37b36a5d875da0a94baea36638
Kulvir Kaur <kulvirk@users.noreply.github.com>
2026-02-22 17:26:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ if uploaded_file is not None:
         st.subheader("Uploaded PDF")
 
         # ⭐ SAFE BUILT-IN VIEWER
-        st.pdf(pdf_bytes)
+        st.pdf(pdf_bytes, key="uploaded_pdf")  #st.pdf(pdf_bytes)
 
     with right:
         st.info("Use scroll / zoom controls inside viewer")
@@ -122,10 +122,11 @@ if "debug_pdf_path" in st.session_state:
     with open(st.session_state["debug_pdf_path"], "rb") as f:
         debug_bytes = f.read()
 
-    st.pdf(debug_bytes)
+    st.pdf(debug_bytes, key="debug_pdf") #st.pdf(debug_bytes)
 
     st.download_button(
         "⬇ Download Debug PDF",
         debug_bytes,
         file_name="debug_output.pdf"
     )
+
